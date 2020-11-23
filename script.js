ee4805e78d3e1a6dafcbdc8190450a3d02ee22d0
Rami Benchouia <ramibenchouia1@gmail.com>
2020-11-23 12:37:04 -0600

    Added some quotes to js, updated CSS to fix navbar aesthetics and landing page layout.

--- script.js
+++ script.js
@@ -4,21 +4,23 @@
 //https://www.freecodecamp.org/news/creating-a-bare-bones-quote-generator-with-javascript-and-html-for-absolute-beginners-5264e1725f08/
 
 var quotes = [
-    'yo -nostradamus',
-    'ello -gordon ramsey',
-    'To the poet and sage, all things are friendly and hallowed, all experiences profitable, all days holy, all men divine. - F. Nietzsche',
-    'fuck ya -lil wayne',
-    'bruh -nick swardson',
-    'my g -jigga',
-    'play wonderwall -R.S.',
+    'To the poet and sage, all things are friendly and hallowed, all experiences profitable, all days holy, all men divine. -F. Nietzsche',
+    'Gimme Oasis Wonderwall, I want that song bro. -R.S.',
     'A man is a god in ruins. -R. W. Emerson',
-    'Yesterday i was clever, so i wanted to change the world. Today i am wise, so i am changing myself. -J. Rumi',
+    'Yesterday I was clever, so I wanted to change the world. Today I am wise, so I am changing myself. -J. Rumi',
     'De gustibus non est disputandum, mi amice',
-    'To those who feel, the world is a tragedy; to those who think the world is a comedy',
+    'To those who feel, the world is a tragedy; to those who think the world is a comedy. -H. Walpole',
     'Properly speaking, nothing has been experienced but what has been lived and made conscious. -A. Camus',
     'One repays a teacher badly if one always remains nothing but a pupil. -F. Nietzsche',
-    'Whereof one cannot speak, thereof one must be silent -L. Wittgenstein'
-
+    'Whereof one cannot speak, thereof one must be silent -L. Wittgenstein',
+    'Dreams are true while they last, and do we not live in dreams? -A. Lord Tennyson',
+    'How many heartfuls of sorrow shall balance a bushel of wheat? How hard a thing is life to the lowly, and yet how human and real! And all this life and love and strife and failure, is it the twilight of nightfall or the flush of some faint-dawning day? -W.E.B. DuBois',
+    'Freedom is a very good horse to ride, but to ride somehwere. -M. Arnold',
+    'Everything was beautiful, and nothing hurt. -K. Vonnegut',
+    'Cultural artistic expression reflects philosophical evolution, interest in growth, perspective, observation, interpretation. -Mordin',
+    'Ain\'t no money like trap money. -Trapmoneybenny',
+    'I fell in love with a white girl, I know that she dont like me back. -KEY!',
+    'He who has learned to see beauty in due order and succession, when he comes toward the end will suddenly perceive a nature of wondrous beauty... beauty absolute, separate, simple, and everlasting, which without diminution and without increase, or any change, is imparted to the ever-growing and perishing beauties of all other things. He who from these, ascending under the influence of true love, begins to perceive that beauty, is not far from the end. -Diotima speaking to Socrates in Plato`s Symposium'
 ]
 
 function newQuote() {
